e510714c192128860fbcdc52a27c725f36e9c45f
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2024-01-03 16:06:24 -0500

    cambios

---
+++ functions/function_subpagina1.js
@@ -0,0 +1,16 @@
+const swiper_Producto = new Swiper('.swiper-Producto_1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    allowTouchMove: true,
+    slidesPerView: 1,
+    spacebetween: 15,
+    slidesPerGroupAuto: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
